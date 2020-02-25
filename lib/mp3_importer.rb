fd1de1abc0cbcefbfe3b63150f71859ab943eb19
norio c <norionorioc@gmail.com>
2020-02-25 02:59:23 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -6,8 +6,19 @@ class MP3Importer
       @path = path
   end
   
-  def files 
-      @files || =
+  def files
+    files = []
+    Dir.new(self.path).each do |file|
+      files << file if file.length > 4
+    end
+    files
+  end
+
+  def import
+    self.files.each do |filename|
+      Song.new_by_filename(filename)
+    end
+  end
 
 
 
