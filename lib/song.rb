dbacf196443408ddd03a793d7a0593f090598267
norio c <norionorioc@gmail.com>
2020-02-25 02:10:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,37 +1,30 @@
-class Artist
+class Song
+  
+  attr_accessor :name, :artist
   @@all = []
-  attr_accessor :name, :songs
 
   def initialize(name)
-      @name = name
+     @name = name
       save
-  end
-
-  def self.all
-      @@all    
-  end
-
-  def add_song(song)
-      song.artist = self
-  end
-
-  def songs
-      Song.all.select {|song| song.artist == self}
-  end
-
-  def self.find_or_create_by_name(name)
-      self.find(name) ? self.find(name) : self.new(name)
-  end
-
-  def self.find(name)
-      self.all.find {|artist| artist.name == name }
   end
 
   def save
       @@all << self
   end
 
-  def print_songs
-      songs.each {|song| puts song.name}
+  def self.all
+      @@all
+  end
+
+  def self.new_by_filename(filename)
+      artist, song = filename.split(" - ")
+      new_song = self.new(song)
+      new_song.artist_name = artist
+      new_song
+  end
+
+  def artist_name=(name)
+      self.artist = Artist.find_or_create_by_name(name)
+      artist.add_song(self)
   end
 end
